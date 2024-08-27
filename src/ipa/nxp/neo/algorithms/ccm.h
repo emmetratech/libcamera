ce6c360c56350152b62e72be73b1d9abc916cd80
Celine Laurencin <celine.laurencin@nxp.com>
2024-08-27 09:04:45 +0200

    ipa: neo: Add Ccm algorithm
    The CSC matrix from the RGB2YUV bloc of the ISP is programmed with
    the resulting merge (multiplication) of both following matrices:
    - the static and standard matrix for the RGB to YUV conversion
    - the color correction matrix interpolated according to the measured color temperature
    
    The CCM algorithm should run after the AWB algorithm
    since it has dependency on the color temperature computed by the AWB.
    
    Signed-off-by: Celine Laurencin <celine.laurencin@nxp.com>

---
+++ src/ipa/nxp/neo/algorithms/ccm.h
@@ -0,0 +1,52 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+/*
+ * Based on RkISP1 Color Correction Matrix control algorithm
+ *     src/ipa/rkisp1/algorithms/ccm.h
+ * Copyright (C) 2024, Ideas On Board
+ *
+ * ccm.h - Color Correction Matrix control algorithm
+ * Copyright 2024 NXP
+ */
+
+#pragma once
+
+#include <linux/nxp_neoisp.h>
+
+#include "libipa/interpolator.h"
+#include "libipa/matrix.h"
+
+#include "algorithm.h"
+
+namespace libcamera {
+
+namespace ipa::nxpneo::algorithms {
+
+class Ccm : public Algorithm
+{
+public:
+	Ccm() {}
+	~Ccm() = default;
+
+	int init(IPAContext &context, const YamlObject &tuningData) override;
+	void prepare(IPAContext &context, const uint32_t frame,
+		     IPAFrameContext &frameContext,
+		     neoisp_meta_params_s *params) override;
+	void process(IPAContext &context, const uint32_t frame,
+		     IPAFrameContext &frameContext,
+		     const neoisp_meta_stats_s *stats,
+		     ControlList &metadata) override;
+
+private:
+	void parseYaml(const YamlObject &tuningData);
+	void setParameters(neoisp_meta_params_s *params,
+			   const Matrix<float, 3, 3> &matrix,
+			   const Matrix<int32_t, 3, 1> &offsets);
+
+	unsigned int ct_;
+	Interpolator<Matrix<float, 3, 3>> ccm_;
+	Interpolator<Matrix<int32_t, 3, 1>> offsets_;
+};
+
+} /* namespace ipa::nxpneo::algorithms */
+
+} /* namespace libcamera */
