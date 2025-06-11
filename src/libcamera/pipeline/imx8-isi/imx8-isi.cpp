emmetratech/libcamera

commit 517862fd31cf1ad652f43c0da718b7709c4e818f

pipeline: imx8-isi: Add ISP subdevice support
This patch adds support of ISP subdevice, which may be inserted between
the camera sensor and the CSI subdevices. ISP is then considered as the
'sensor' element in the pipeline (similar approach is used in simple
pipeline handler driver).

Sensor's source pad index is dynamically computed, and stored in CameraData
element. It was previously tied to 0 because camera sensors only have 1
source pad, but this is not valid anymore for and ISP sensor, which may
have several sink a/o source pads.

Signed-off-by: Antoine Bouyer <antoine.bouyer@nxp.com>

src/libcamera/pipeline/imx8-isi/imx8-isi.cpp
@@ -72,6 +72,7 @@ public:
 	std::vector<Stream *> enabledStreams_;
 
 	unsigned int xbarSink_;
+	unsigned int sensorSourcePadIdx_;
 };
 
 class ISICameraConfiguration : public CameraConfiguration
@@ -820,8 +821,10 @@ int PipelineHandlerISI::configure(Camera *camera, CameraConfiguration *c)
 	ISICameraConfiguration *camConfig = static_cast<ISICameraConfiguration *>(c);
 	ISICameraData *data = cameraData(camera);
 
-	/* All links are immutable except the sensor -> csis link. */
-	const MediaPad *sensorSrc = data->sensor_->entity()->getPadByIndex(0);
+	/*
+	 * All links are immutable except the sensor/isp -> csis link.
+	 */
+	const MediaPad *sensorSrc = data->sensor_->entity()->getPadByIndex(data->sensorSourcePadIdx_);
 	sensorSrc->links()[0]->setEnabled(true);
 
 	/*
@@ -1041,7 +1044,7 @@ bool PipelineHandlerISI::match(DeviceEnumerator *enumerator)
 	for (MediaPad *pad : crossbar_->entity()->pads()) {
 		unsigned int sink = numSinks;
 
-		if (!(pad->flags() & MEDIA_PAD_FL_SINK) || pad->links().empty())
+		if (!(pad->flags() & MEDIA_PAD_FL_SINK))
 			continue;
 
 		/*
@@ -1076,11 +1079,22 @@ bool PipelineHandlerISI::match(DeviceEnumerator *enumerator)
 		if (!(pad->flags() & MEDIA_PAD_FL_SINK) || pad->links().empty())
 			continue;
 
+		/* Sensor - When present, ISP is considerred as a sensor from pipeline point of view. */
 		MediaEntity *sensor = pad->links()[0]->source()->entity();
-		if (sensor->function() != MEDIA_ENT_F_CAM_SENSOR) {
+		if (sensor->function() != MEDIA_ENT_F_CAM_SENSOR && sensor->function() != MEDIA_ENT_F_PROC_VIDEO_ISP) {
 			LOG(ISI, Debug) << "Skip unsupported subdevice "
 					<< sensor->name();
 			continue;
+		}
+
+		unsigned int sensorSourcePadIx = 0;
+		for (MediaPad *sensorPad : sensor->pads()) {
+			if (!(sensorPad->flags() & MEDIA_PAD_FL_SOURCE) || sensorPad->links().empty())
+				/*
+				* Count each sensor pad to enable the one
+				* currently used in the pipeline.
+				*/
+				sensorSourcePadIx++;
 		}
 
 		/* Create the camera data. */
@@ -1097,6 +1111,8 @@ bool PipelineHandlerISI::match(DeviceEnumerator *enumerator)
 		 */
 		if (formatter)
 			data->formatter_ = std::make_unique<V4L2Subdevice>(formatter);
+
+		data->sensorSourcePadIdx_ = sensorSourcePadIx;
 
 		ret = data->init();
 		if (ret) {

commit f0aab6ecde34e6d71214ae7ca122e7713d992b69

pipeline: imx8-isi: Fix sensor source pad indexing issue
When computing the sensor source pad index, it is assumed that sink pads
are listed _before_ source pads. Thus there is a loop over all sink pads
first, incrementing index counter until we reach source pads, then we stop
incrementing until the end of the loop. Resulting index is as expected,
corresponding to first source pad index.

However, if source pads are listed first, then the computed index is not
set as expected: all source pads are first skipped, index stays 0, then
all sink pads keep incrementing index counter. Resulting index would then
point to one of the sink pads, instead of the first source pad, resulting
in a crash when configuring sensor.

Breaking the loop when first source is detected prevents such issue.

Fixes: 517862fd31cf ("pipeline: imx8-isi: Add ISP subdevice support")
Signed-off-by: Antoine Bouyer <antoine.bouyer@nxp.com>

src/libcamera/pipeline/imx8-isi/imx8-isi.cpp
@@ -1124,14 +1124,16 @@ bool PipelineHandlerISI::match(DeviceEnumerator *enumerator)
 			continue;
 		}
 
-		unsigned int sensorSourcePadIx = 0;
+		unsigned int sensorSourcePadIdx = 0;
 		for (MediaPad *sensorPad : sensor->pads()) {
 			if (!(sensorPad->flags() & MEDIA_PAD_FL_SOURCE) || sensorPad->links().empty())
 				/*
 				* Count each sensor pad to enable the one
 				* currently used in the pipeline.
 				*/
-				sensorSourcePadIx++;
+				sensorSourcePadIdx++;
+			else
+				break;
 		}
 
 		/*
@@ -1148,7 +1150,7 @@ bool PipelineHandlerISI::match(DeviceEnumerator *enumerator)
 		data->sensor_ = CameraSensorFactoryBase::create(sensor);
 		data->csis_ = std::make_unique<V4L2Subdevice>(csi);
 		data->xbarSink_ = sink;
-		data->sensorSourcePadIdx_ = sensorSourcePadIx;
+		data->sensorSourcePadIdx_ = sensorSourcePadIdx;
 		data->pipeOffset_ = numCameras * data->streams_.size();
 
 		LOG(ISI, Debug)