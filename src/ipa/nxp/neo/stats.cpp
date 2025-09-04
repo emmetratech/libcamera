emmetratech/libcamera

commit 38b0a1645cba757c084d042b59ae4cf248db1a78

ipa: neo: Add ISP extended statistics class
Signed-off-by: Aymen Sghaier <aymen.sghaier@nxp.com>
Signed-off-by: Antoine Bouyer <antoine.bouyer@nxp.com>

src/ipa/nxp/neo/stats.cpp
@@ -1,0 +1,160 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+/*
+ * Copyright (C) 2024, Ideas On Board
+ * Copyright 2025 NXP
+ *
+ * NXP NEOISP Statistics
+ */
+
+#include "stats.h"
+
+#include <map>
+#include <stddef.h>
+#include <string.h>
+
+#include <linux/nxp_neoisp.h>
+#include <linux/videodev2.h>
+
+#include <libcamera/base/log.h>
+#include <libcamera/base/utils.h>
+
+namespace libcamera {
+
+LOG_DEFINE_CATEGORY(NxpNeoStats)
+
+namespace ipa::nxpneo {
+
+namespace {
+
+struct BlockStatsTypeInfo {
+	enum neoisp_stats_block_type_e type;
+	size_t size;
+	size_t offset;
+};
+
+#define NXPNEO_BLOCK_STATS_TYPE_ENTRY(block, id, category, type, member)       \
+	{                                                                      \
+		BlockStatsType::block,                                         \
+		{                                                              \
+			NEOISP_STATS_BLK_##id,                                 \
+			sizeof(struct neoisp_##type##_stats_s),                \
+			offsetof(struct neoisp_meta_stats_s, category.member), \
+		}                                                              \
+	}
+
+const std::map<BlockStatsType, BlockStatsTypeInfo> kBlockTypeInfo = {
+	NXPNEO_BLOCK_STATS_TYPE_ENTRY(RCTemp, RCTEMP, regs, ctemp_reg, ct),
+	NXPNEO_BLOCK_STATS_TYPE_ENTRY(RDrc, RDRC, regs, drc_reg, drc),
+	NXPNEO_BLOCK_STATS_TYPE_ENTRY(RAf, RAF, regs, af_reg, af),
+	NXPNEO_BLOCK_STATS_TYPE_ENTRY(RBnr, RBNR, regs, bnr_reg, bnr),
+	NXPNEO_BLOCK_STATS_TYPE_ENTRY(RNr, RNR, regs, nr_reg, nr),
+	NXPNEO_BLOCK_STATS_TYPE_ENTRY(REe, REE, regs, ee_reg, ee),
+	NXPNEO_BLOCK_STATS_TYPE_ENTRY(RDf, RDF, regs, df_reg, df),
+	NXPNEO_BLOCK_STATS_TYPE_ENTRY(MCTemp, MCTEMP, mems, ctemp_mem, ctemp),
+	NXPNEO_BLOCK_STATS_TYPE_ENTRY(MRgbIr, MRGBIR, mems, rgbir_mem, rgbir),
+	NXPNEO_BLOCK_STATS_TYPE_ENTRY(MHist, MHIST, mems, hist_mem, hist),
+	NXPNEO_BLOCK_STATS_TYPE_ENTRY(MDrc, MDRC, mems, drc_mem, drc),
+};
+
+} /* namespace */
+
+NxpNeoStatsBlockBase::NxpNeoStatsBlockBase(NxpNeoStats *stats,
+					   BlockStatsType type, const Span<uint8_t> &data)
+	: stats_(stats), type_(type)
+{
+	if (stats_->isExtensible()) {
+		header_ = data.subspan(0, sizeof(neoisp_ext_stats_block_header_s));
+		data_ = data.subspan(sizeof(neoisp_ext_stats_block_header_s));
+	} else {
+		data_ = data;
+	}
+}
+
+NxpNeoStats::NxpNeoStats(uint32_t apiVersion, Span<uint8_t> data)
+	: apiVersion_(apiVersion), data_(data), used_(0)
+{
+	uint32_t offset = 0;
+
+	if (this->isExtensible()) {
+		struct neoisp_ext_stats_s *stats =
+			reinterpret_cast<struct neoisp_ext_stats_s *>(data.data());
+
+		used_ = stats->data_size;
+		used_ += offsetof(struct neoisp_ext_stats_s, data);
+
+		offset = offsetof(struct neoisp_ext_stats_s, data);
+
+		/* Parse received blocks. */
+		while (offset < stats->data_size) {
+			Span<uint8_t> blk = data.subspan(offset);
+
+			auto hdr = reinterpret_cast<struct neoisp_ext_stats_block_header_s *>(blk.data());
+			BlockStatsType type = static_cast<BlockStatsType>(hdr->type);
+
+			auto infoIt = kBlockTypeInfo.find(type);
+			if (infoIt == kBlockTypeInfo.end()) {
+				LOG(NxpNeoStats, Error)
+					<< "Invalid statistics block type "
+					<< utils::to_underlying(type);
+				break;
+			}
+			const BlockStatsTypeInfo &info = infoIt->second;
+
+			/* Check we received expected size, and that we are 8-bytes aligned. */
+			size_t size = (info.size + sizeof(struct neoisp_ext_params_block_header_s) + 7) & ~7;
+			if (size != hdr->size) {
+				LOG(NxpNeoStats, Error)
+					<< "Invalid statistics block size " << hdr->size
+					<< ", expected " << size
+					<< ", for type " << utils::to_underlying(type);
+				break;
+			}
+
+			blocks_[type] = blk.subspan(0, hdr->size);
+			offset += hdr->size;
+
+			if (hdr->size == 0)
+				break;
+		}
+	} else {
+		used_ = sizeof(struct neoisp_meta_stats_s);
+	}
+}
+
+Span<uint8_t> NxpNeoStats::block(BlockStatsType type) const
+{
+	auto infoIt = kBlockTypeInfo.find(type);
+	if (infoIt == kBlockTypeInfo.end()) {
+		LOG(NxpNeoStats, Error)
+			<< "Invalid parameters block type "
+			<< utils::to_underlying(type);
+		return {};
+	}
+
+	const BlockStatsTypeInfo &info = infoIt->second;
+
+	/*
+	 * For the legacy format, return a block referencing the fixed location
+	 * and size of the data.
+	 */
+	if (!this->isExtensible())
+		return data_.subspan(info.offset, info.size);
+
+	/*
+	 * For the extensible format, look for the requested block, including
+	 * the header.
+	 */
+	auto cacheIt = blocks_.find(type);
+	if (cacheIt == blocks_.end()) {
+		LOG(NxpNeoStats, Error)
+			<< "No block found with type "
+			<< utils::to_underlying(type);
+		return {};
+	}
+
+	return cacheIt->second;
+}
+
+} /* namespace ipa::nxpneo */
+
+} /* namespace libcamera */

src/ipa/nxp/neo/meson.build
@@ -9,6 +9,7 @@ neo_ipa_sources = files([
     'ipa_context.cpp',
     'neo_ipa.cpp',
     'params.cpp',
+    'stats.cpp',
 ])
 
 neo_ipa_sources += neo_ipa_algorithms

src/ipa/nxp/neo/stats.h
@@ -1,0 +1,151 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+/*
+ * Copyright (C) 2024, Ideas On Board
+ * Copyright 2025 NXP
+ *
+ * NXP NEOISP Statistics
+ */
+
+#pragma once
+
+#include <map>
+#include <stdint.h>
+
+#include <linux/nxp_neoisp.h>
+
+#include <libcamera/base/class.h>
+#include <libcamera/base/span.h>
+
+namespace libcamera {
+
+namespace ipa::nxpneo {
+
+enum class BlockStatsType {
+	RCTemp,
+	RDrc,
+	RAf,
+	RBnr,
+	RNr,
+	REe,
+	RDf,
+	MCTemp,
+	MRgbIr,
+	MHist,
+	MDrc,
+};
+
+namespace details {
+
+template<BlockStatsType S>
+struct block_stats_type {
+};
+
+#define NXPNEO_DEFINE_BLOCK_STATS_TYPE(blockType, blockStruct)      \
+	template<>                                                  \
+	struct block_stats_type<BlockStatsType::blockType> {        \
+		using type = struct neoisp_##blockStruct##_stats_s; \
+	};
+
+NXPNEO_DEFINE_BLOCK_STATS_TYPE(RCTemp, ctemp_reg)
+NXPNEO_DEFINE_BLOCK_STATS_TYPE(RDrc, drc_reg)
+NXPNEO_DEFINE_BLOCK_STATS_TYPE(RAf, af_reg)
+NXPNEO_DEFINE_BLOCK_STATS_TYPE(RBnr, bnr_reg)
+NXPNEO_DEFINE_BLOCK_STATS_TYPE(RNr, nr_reg)
+NXPNEO_DEFINE_BLOCK_STATS_TYPE(REe, ee_reg)
+NXPNEO_DEFINE_BLOCK_STATS_TYPE(RDf, df_reg)
+NXPNEO_DEFINE_BLOCK_STATS_TYPE(MCTemp, ctemp_mem)
+NXPNEO_DEFINE_BLOCK_STATS_TYPE(MRgbIr, rgbir_mem)
+NXPNEO_DEFINE_BLOCK_STATS_TYPE(MHist, hist_mem)
+NXPNEO_DEFINE_BLOCK_STATS_TYPE(MDrc, drc_mem)
+
+} /* namespace details */
+
+class NxpNeoStats;
+
+class NxpNeoStatsBlockBase
+{
+public:
+	NxpNeoStatsBlockBase(NxpNeoStats *stats, BlockStatsType type,
+			     const Span<uint8_t> &data);
+
+	Span<uint8_t> data() const { return data_; }
+
+private:
+	LIBCAMERA_DISABLE_COPY(NxpNeoStatsBlockBase)
+
+	NxpNeoStats *stats_;
+	BlockStatsType type_;
+	Span<uint8_t> header_;
+	Span<uint8_t> data_;
+};
+
+template<BlockStatsType S>
+class NxpNeoStatsBlock : public NxpNeoStatsBlockBase
+{
+public:
+	using Type = typename details::block_stats_type<S>::type;
+
+	NxpNeoStatsBlock(NxpNeoStats *stats, const Span<uint8_t> &data)
+		: NxpNeoStatsBlockBase(stats, S, data)
+	{
+	}
+
+	const Type *operator->() const
+	{
+		return reinterpret_cast<const Type *>(data().data());
+	}
+
+	Type *operator->()
+	{
+		return reinterpret_cast<Type *>(data().data());
+	}
+
+	const Type &operator*() const &
+	{
+		return *reinterpret_cast<const Type *>(data().data());
+	}
+
+	Type &operator*() &
+	{
+		return *reinterpret_cast<Type *>(data().data());
+	}
+
+	Type *stats()
+	{
+		return reinterpret_cast<Type *>(data().data());
+	}
+};
+
+class NxpNeoStats
+{
+public:
+	NxpNeoStats(uint32_t apiVersion, Span<uint8_t> data);
+
+	template<BlockStatsType S>
+	NxpNeoStatsBlock<S> block() const
+	{
+		return NxpNeoStatsBlock<S>(const_cast<NxpNeoStats *>(this), block(S));
+	}
+
+	size_t size() const { return used_; }
+
+private:
+	friend class NxpNeoStatsBlockBase;
+
+	Span<uint8_t> block(BlockStatsType type) const;
+	bool isExtensible() const
+	{
+		return apiVersion_ != NEOISP_LEGACY_META_BUFFER;
+	}
+
+	uint32_t apiVersion_;
+
+	Span<uint8_t> data_;
+	size_t used_;
+
+	std::map<BlockStatsType, Span<uint8_t>> blocks_;
+};
+
+} /* namespace ipa::nxpneo */
+
+} /* namespace libcamera */

commit db631eae97f22c24d1aca0633911a8c22811350b

ipa: neo: stats: Use correct header size
This is a non functional change, because both stats and params header
have the same length. But it is still better to use the appropriate
structure in case a field is added/removed/changed in future.

Fixes: 38b0a1645cba ("ipa: neo: Add ISP extended statistics class")
Signed-off-by: Antoine Bouyer <antoine.bouyer@nxp.com>

src/ipa/nxp/neo/stats.cpp
@@ -101,7 +101,7 @@ NxpNeoStats::NxpNeoStats(uint32_t apiVersion, Span<uint8_t> data)
 			const BlockStatsTypeInfo &info = infoIt->second;
 
 			/* Check we received expected size, and that we are 8-bytes aligned. */
-			size_t size = (info.size + sizeof(struct neoisp_ext_params_block_header_s) + 7) & ~7;
+			size_t size = (info.size + sizeof(struct neoisp_ext_stats_block_header_s) + 7) & ~7;
 			if (size != hdr->size) {
 				LOG(NxpNeoStats, Error)
 					<< "Invalid statistics block size " << hdr->size