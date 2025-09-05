emmetratech/libcamera

commit 2e7fde0ae2e7cf214269c7a2ee33598ce93a87b4

ipa: neo: rgbir: Correct detection of IR output format
This patch fixes the detection of the IR output format
8/16 bits.
The existing check of format was applying to all the streams.
However if among the streams, the frame stream is configured to
the monochrome format Y8 or Y16, this results in wrong detection.

This patch is now checking the format for the IR stream only.

Signed-off-by: Celine Laurencin <celine.laurencin@nxp.com>

src/ipa/nxp/neo/algorithms/rgbir.cpp
@@ -163,14 +163,10 @@ void RgbIr::prepare([[maybe_unused]] IPAContext &context, const uint32_t frame,
 		<< rgbirConfig->ccm1_th_threshold << " "
 		<< rgbirConfig->ccm2_th_threshold;
 
-	/* Look for 8 or 16 bits IR stream, and defaults to 8 bits */
+	/* Look for 8 or 16 bits IR stream, and defaults to 8 bits. */
 	bool irStream16bits = false;
-	std::vector<IPAStream> &streams = context.configuration.streams;
-	auto iter = std::find_if(streams.begin(), streams.end(),
-				 [](auto &stream) {
-					 return stream.pixelFormat == formats::R16.fourcc();
-				 });
-	if (iter != streams.end())
+	IPAStream streamIr = context.configuration.streams[IPAStreamTypeIr];
+	if (streamIr.pixelFormat == formats::R16.fourcc())
 		irStream16bits = true;
 
 	/* IR Compression configuration */

commit ff880062c168b1597f2e6e20c54840b62edadf5c

ipa: neo: Fix infrared stream only operation
Current implementation segfaults when running a pipeline for a RGBIr
sensor with a single stream for IR without RGB/YUV stream.
Reason is that IPA accesses IPAStream entry from a vector of streams
configuration assuming that both streams are populated.

This change stores the IPAStream configuration locally in the IPA in a
map of IPAStreamType, as passed over the IPA interface. That way, the
presence of the IR stream configuration entry can be tested before its
dereference.

Fixes: 2e7fde0ae2e7 ("ipa: neo: rgbir: Correct detection of IR output format")
Signed-off-by: Julien Vuillaumier <julien.vuillaumier@nxp.com>

src/ipa/nxp/neo/algorithms/rgbir.cpp
@@ -165,9 +165,12 @@ void RgbIr::prepare([[maybe_unused]] IPAContext &context, const uint32_t frame,
 
 	/* Look for 8 or 16 bits IR stream, and defaults to 8 bits. */
 	bool irStream16bits = false;
-	IPAStream streamIr = context.configuration.streams[IPAStreamTypeIr];
-	if (streamIr.pixelFormat == formats::R16.fourcc())
-		irStream16bits = true;
+	auto it = context.configuration.streams.find(IPAStreamTypeIr);
+	if (it != context.configuration.streams.end()) {
+		IPAStream &streamIr = it->second;
+		if (streamIr.pixelFormat == formats::R16.fourcc())
+			irStream16bits = true;
+	}
 
 	/* IR Compression configuration */
 	RgbIr::IrCompression &comp =

src/ipa/nxp/neo/ipa_context.h
@@ -63,7 +63,7 @@ struct IPASessionConfiguration {
 		struct neoisp_roi_cfg_s roi;
 	} drc;
 
-	std::vector<IPAStream> streams;
+	std::map<IPAStreamType, IPAStream> streams;
 
 	IPAColorSpace colorSpace;
 };

src/ipa/nxp/neo/neo_ipa.cpp
@@ -290,9 +290,9 @@ int IPANxpNeo::configure(const IPAConfigInfo &ipaConfig,
 	context_.configuration.sensor.bpp = bpp;
 
 	/* Active streams */
-	std::vector<IPAStream> &streams = context_.configuration.streams;
-	for (auto it = streamConfig.begin(); it != streamConfig.end(); it++)
-		streams.push_back(it->second);
+	std::map<IPAStreamType, IPAStream> &streams = context_.configuration.streams;
+	for (const auto &[streamType, config] : streamConfig)
+		streams[static_cast<IPAStreamType>(streamType)] = config;
 
 	context_.configuration.colorSpace = colorSpace;
 