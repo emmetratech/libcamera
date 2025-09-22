emmetratech/libcamera

commit f79d4062452783c1b8deb4a341d05d5e9aaac6a5

ipa: neo: hdr_decomp: Add mode-dependent default configuration
In present implementation, HDR Decomp configuration relies on the ISP
driver default configuration where linear decompression (gain) is used.
To support HDR and non-HDR modes with the same calibration file, the
algorithm has to configure a linear configuration dependent on the
targetted mode, independently from the ISP driver:
- non-HDR-merge: linear mode of operation, assuming
  PIPECONF.LPALIGN0/1=1 has been set. Thus, target input0/input1 format
  of 20-bit/16-bit is already present at the input of the HDR Decomp
  block and bypass configuration can be used. Exception is ISP harware
  revision 2 with 12-bit input format: 16-bit to 20-bit conversion is
  required with a 2^^4 gain.
- HDR-merge: we want to keep the native pixel format at the output of
  the HDR Decomp block, so PIPECONF.LPALIGN0/1=0 is assumed. In most
  cases a bypass configuration is needed. Exception is also ISP hardware
  revision 2 with 12-but input format: 16-bit to 12-bit conversion is
  required with a 2^^(-4) gain.

Signed-off-by: Julien Vuillaumier <julien.vuillaumier@nxp.com>

src/ipa/nxp/neo/algorithms/hdr_decomp.cpp
@@ -31,14 +31,58 @@ namespace ipa::nxpneo::algorithms {
  * The block can be used to apply a non-linear decompression of the pixel
  * values when the sensor uses compression. It may also be used for simple
  * linear rescaling of the pixel values.
- * The target output pixel format is:
+ *
+ *       input0              input1
+ *     AXI IN0 DMA         AXI IN1 DMA
+ *          │                   │
+ *  ┌───────▼───────────────────▼───────┐
+ *  │ PIPECONF                          │
+ *  │    LPALIGN0             LPALIGN1  │
+ *  │    INALIGN0             INALIGN1  │
+ *  └───────┬───────────────────┬───────┘
+ *  ┌───────▼───────┐   ┌───────▼───────┐
+ *  │      HC0      │   │      HC1      │
+ *  └───────┬───────┘   └───────┬───────┘
+ *  ┌───────▼───────┐   ┌───────▼───────┐
+ *  │  HDR Decomp0  │   │  HDR Decomp1  │
+ *  └───────┬───────┘   └───────┬───────┘
+ *  ┌───────▼───────┐   ┌───────▼───────┐
+ *  │     OBWB0     │   │     OBWB1     │
+ *  └───────┬───────┘   └───────┬───────┘
+ *  ┌───────▼───────────────────▼───────┐
+ *  │             HDR Merge             │
+ *  └─────────────────┬─────────────────┘
+ *  ┌─────────────────▼─────────────────┐
+ *  │               RGBIR               │
+ *  └───────┬───────────────────┬───────┘
+ *  ┌───────▼───────┐           │
+ *  │     OBWB2     │           │
+ *  └───────┬───────┘           │
+ *          ▼                   ▼
+ *      to RGB Path        to IR path
+ *
+ * Input format to the HDR Decompression block is either the native sensor
+ * output pixel format or a MSB-aligned shifted version of it - see PIPECONF
+ * block LPALIGN0/1 configurations.
+ * When a non-linear decompression is to be applied the input pixel format of
+ * the block should be limited to 16-bit format to be able to configure some
+ * knee-points. In that case the LPALIGN0/1 configuration should typically be
+ * set to zero explicitly in the calibration file to avoid rescaling the native
+ * pixel format.
+ *
+ * If the HDR merge block is not used, the target pixel format at the output of
+ * the HDR Decompression block is:
  * - 20-bit on line path 0 (input0)
  * - 16-bit on line path 1 (input1)
- * Input format is either the sensor output bpp or a MSB-aligned shifted version
- * of it - see PIPECONF block LPALIGN0 and LPALIGN1 configurations.
- * HDR DECOMP operation is configured by a number of parameters defined in the
- * sensor calibration file.
- * Using points[] evaluated in increasing order, the conversion logic is:
+ * When the HDR block is used, the target pixel format at the output of the HDR
+ * Decompression blocks is the same as the native sensor format. Thus the block
+ * is configured as bypass with a unitary gain.
+ *
+ * When the HDR Decompression block is explicitly configured in the calibration
+ * file, those values are applied with priority.
+ * For explicit user-defined configuration of the block from the configuration
+ * file, a number of parameters are defined in the sensor calibration file.
+ * Using points[] evaluated in increasing order, the block conversion logic is:
  * if (pv < points[N])
  *   opv = (pv - offsets[N-1]) * ratios[N-1] + newpoints[N-1]
  * with:
@@ -50,14 +94,24 @@ namespace ipa::nxpneo::algorithms {
  * - ratios: KNEE_RATIO[0-4] (u7.5)
  * Last entry in the offsets/newpoints/ratios arrays is used as the default case
  * when no value from points[] array matched the condition (pv < points[N]).
+ *
+ * If no block configuration is present in the calibration file, the algorithm
+ * falls back on a default configuration logic, and the block will be configured
+ * as simple linear gain or bypass, without decompression.
+ * If pipeline is not in HDR-merge mode, it is assumed that PIPECONF.LPALIGN0/1
+ * is set to 1 meaning that camera pixel native format has been rescaled. Most
+ * of the time block can be used in bypass, as the input0 and input1 formats
+ * already match the HDR Decompression block targeted output format that is
+ * 20-bit for input0 and 16-bit for input1.
+ * In HDR-merge mode of operation, it is assumed that PIPECONF.LPALIGN0/1 has
+ * been set to zero to avoid rescaling. Native sensor format is expected at the
+ * output of the HDR Decompression block so it can be configured in bypass mode.
+ * See the PipeConf algorithm for PIPECONF block rescaling logic and exceptions,
+ * depending on PIPECONF.LPALIGN0/1, the sensor native pixel format and the ISP
+ * hardware revision.
  */
 
 LOG_DEFINE_CATEGORY(NxpNeoAlgoHdrDecomp)
-
-HdrDecomp::HdrDecomp()
-	: input0_({}), input1_({})
-{
-}
 
 /**
  * \copydoc libcamera::ipa::Algorithm::init
@@ -70,96 +124,128 @@ int HdrDecomp::init([[maybe_unused]] IPAContext &context,
 	 */
 
 	const YamlObject &obj0 = tuningData["input0"];
-	if (!obj0.isDictionary() || (!obj0.size())) {
-		LOG(NxpNeoAlgoHdrDecomp, Debug) << "input0 not configured";
-		return 0;
+	if (obj0.isDictionary() || (obj0.size())) {
+		std::optional<std::vector<uint16_t>> points =
+			obj0["points"].getList<uint16_t>();
+		if (points && points->size() != kNumPoints) {
+			LOG(NxpNeoAlgoHdrDecomp, Error)
+				<< "input0 points list size must be " << kNumPoints;
+			return -EINVAL;
+		}
+
+		std::optional<std::vector<uint16_t>> offsets =
+			obj0["offsets"].getList<uint16_t>();
+		if (offsets && offsets->size() != kNumOffsets) {
+			LOG(NxpNeoAlgoHdrDecomp, Error)
+				<< "input0 offsets list size must be " << kNumOffsets;
+			return -EINVAL;
+		}
+
+		std::optional<std::vector<uint32_t>> newpoints =
+			obj0["newpoints"].getList<uint32_t>();
+		if (newpoints && newpoints->size() != kNumNewPoints) {
+			LOG(NxpNeoAlgoHdrDecomp, Error)
+				<< "input0 newpoints list size must be " << kNumNewPoints;
+			return -EINVAL;
+		}
+
+		std::optional<std::vector<uint16_t>> ratios =
+			obj0["ratios"].getList<uint16_t>();
+		if (ratios && ratios->size() != kNumRatios) {
+			LOG(NxpNeoAlgoHdrDecomp, Error)
+				<< "input0 ratios list size must be " << kNumRatios;
+			return -EINVAL;
+		}
+
+		if (points && offsets && newpoints && ratios) {
+			input0_.points = std::move(points.value());
+			input0_.offsets = std::move(offsets.value());
+			input0_.newpoints = std::move(newpoints.value());
+			input0_.ratios = std::move(ratios.value());
+			input0_.userConfig = true;
+		}
 	}
 
-	input0_.points = obj0["points"].getList<uint16_t>()
-				.value_or(std::vector<uint16_t>{});
-	if (input0_.points.size() != kNumPoints) {
-		LOG(NxpNeoAlgoHdrDecomp, Error)
-			<< "input0 points list size must be " << kNumPoints;
-		return -EINVAL;
+	if (!input0_.userConfig) {
+		/* Configure block as bypass - unitary gain in u7.5 format. */
+		input0_.points = { 0, 0, 0, 0 };
+		input0_.offsets = { 0, 0, 0, 0, 0 };
+		input0_.newpoints = { 0, 0, 0, 0, 0 };
+		input0_.ratios = { 0, 0, 0, 0, (1 << 5) };
 	}
-
-	input0_.offsets = obj0["offsets"].getList<uint16_t>()
-				.value_or(std::vector<uint16_t>{});
-	if (input0_.offsets.size() != kNumOffsets) {
-		LOG(NxpNeoAlgoHdrDecomp, Error)
-			<< "input0 offsets list size must be " << kNumOffsets;
-		return -EINVAL;
-	}
-
-	input0_.newpoints = obj0["newpoints"].getList<uint32_t>()
-				.value_or(std::vector<uint32_t>{});
-	if (input0_.newpoints.size() != kNumNewPoints) {
-		LOG(NxpNeoAlgoHdrDecomp, Error)
-			<< "input0 newpoints list size must be " << kNumNewPoints;
-		return -EINVAL;
-	}
-
-	input0_.ratios = obj0["ratios"].getList<uint16_t>()
-				.value_or(std::vector<uint16_t>{});
-	if (input0_.ratios.size() != kNumRatios) {
-		LOG(NxpNeoAlgoHdrDecomp, Error)
-			<< "input0 ratios list size must be " << kNumRatios;
-		return -EINVAL;
-	}
-
-	input0_.enabled = true;
 
 	/*
 	 * Input1 calibration parsing
 	 */
 
 	const YamlObject &obj1 = tuningData["input1"];
-	if (!obj1.isDictionary() || (!obj1.size())) {
-		LOG(NxpNeoAlgoHdrDecomp, Debug) << "input1 not configured";
-		return 0;
+	if (obj1.isDictionary() || (obj1.size())) {
+		std::optional<std::vector<uint16_t>> points =
+			obj1["points"].getList<uint16_t>();
+		if (points && points->size() != kNumPoints) {
+			LOG(NxpNeoAlgoHdrDecomp, Error)
+				<< "input1 points list size must be " << kNumPoints;
+			return -EINVAL;
+		}
+
+		std::optional<std::vector<uint16_t>> offsets =
+			obj1["offsets"].getList<uint16_t>();
+		if (offsets && offsets->size() != kNumOffsets) {
+			LOG(NxpNeoAlgoHdrDecomp, Error)
+				<< "input1 offsets list size must be " << kNumOffsets;
+			return -EINVAL;
+		}
+
+		std::optional<std::vector<uint16_t>> newpoints =
+			obj1["newpoints"].getList<uint16_t>();
+		if (newpoints && newpoints->size() != kNumNewPoints) {
+			LOG(NxpNeoAlgoHdrDecomp, Error)
+				<< "input1 newpoints list size must be " << kNumNewPoints;
+			return -EINVAL;
+		}
+
+		std::optional<std::vector<uint16_t>> ratios =
+			obj1["ratios"].getList<uint16_t>();
+		if (ratios && ratios->size() != kNumRatios) {
+			LOG(NxpNeoAlgoHdrDecomp, Error)
+				<< "input1 ratios list size must be " << kNumRatios;
+			return -EINVAL;
+		}
+
+		if (points && offsets && newpoints && ratios) {
+			input1_.points = std::move(points.value());
+			input1_.offsets = std::move(offsets.value());
+			input1_.newpoints = std::move(newpoints.value());
+			input1_.ratios = std::move(ratios.value());
+			input1_.userConfig = true;
+		}
 	}
 
-	input1_.points = obj1["points"].getList<uint16_t>()
-				.value_or(std::vector<uint16_t>{});
-	if (input1_.points.size() != kNumPoints) {
-		LOG(NxpNeoAlgoHdrDecomp, Error)
-			<< "input1 points list size must be " << kNumPoints;
-		return -EINVAL;
+	if (!input1_.userConfig) {
+		/* Configure block as bypass - unitary gain in u7.5 format. */
+		input1_.points = { 0, 0, 0, 0 };
+		input1_.offsets = { 0, 0, 0, 0, 0 };
+		input1_.newpoints = { 0, 0, 0, 0, 0 };
+		input1_.ratios = { 0, 0, 0, 0, (1 << 5) };
 	}
 
-	input1_.offsets = obj1["offsets"].getList<uint16_t>()
-				.value_or(std::vector<uint16_t>{});
-	if (input1_.offsets.size() != kNumOffsets) {
-		LOG(NxpNeoAlgoHdrDecomp, Error)
-			<< "input1 offsets list size must be " << kNumOffsets;
-		return -EINVAL;
-	}
+	return 0;
+}
 
-	input1_.newpoints = obj1["newpoints"].getList<uint16_t>()
-				.value_or(std::vector<uint16_t>{});
-	if (input1_.newpoints.size() != kNumNewPoints) {
-		LOG(NxpNeoAlgoHdrDecomp, Error)
-			<< "input1 newpoints list size must be " << kNumNewPoints;
-		return -EINVAL;
-	}
-
-	input1_.ratios = obj1["ratios"].getList<uint16_t>()
-				.value_or(std::vector<uint16_t>{});
-	if (input1_.ratios.size() != kNumRatios) {
-		LOG(NxpNeoAlgoHdrDecomp, Error)
-			<< "input1 ratios list size must be " << kNumRatios;
-		return -EINVAL;
-	}
-
-	input1_.enabled = true;
-
+/**
+ * \copydoc libcamera::ipa::Algorithm::configure
+ */
+int HdrDecomp::configure([[maybe_unused]] IPAContext &context,
+			 const IPACameraSensorInfo &configInfo)
+{
+	bitsPerPixel_ = configInfo.bitsPerPixel;
 	return 0;
 }
 
 /**
  * \copydoc libcamera::ipa::Algorithm::prepare
  */
-void HdrDecomp::prepare([[maybe_unused]] IPAContext &context, const uint32_t frame,
+void HdrDecomp::prepare(IPAContext &context, const uint32_t frame,
 			[[maybe_unused]] IPAFrameContext &frameContext,
 			NxpNeoParams *params)
 {
@@ -167,67 +253,95 @@ void HdrDecomp::prepare([[maybe_unused]] IPAContext &context, const uint32_t fra
 		return;
 
 	LOG(NxpNeoAlgoHdrDecomp, Debug)
-		<< "input0/1 enabled " << input0_.enabled << "/" << input1_.enabled;
+		<< "input0/1 user config "
+		<< input0_.userConfig << "/" << input1_.userConfig;
 
-	if (input0_.enabled) {
-		auto hdrdec0Config = params->block<BlockParamsType::HdrDec0>();
-		hdrdec0Config.setUpdate(true);
+	/*
+	 * When no user configuration is present in the configuration file we
+	 * fallback to a default linear bypass configuration of the block.
+	 * There is an hardware peculiarity in the ISP hardware revision V2
+	 * related to PIPECONF.LPALIGN0 setting with 12-bit sensor pixel format:
+	 * - Rescaling is done to 16-bit instead of 20-bit for other sensor
+	 *   formats
+	 * - Rescaling is applied event though LPALIGN=0
+	 * This leads to 2 exceptions on this ISP revision V2 with input0 12-bit
+	 * pixel format:
+	 * 1) In non HDR-merge mode, we rely on PIPECONF.LPALIGN0/1 to rescale
+	 *    the pixels to 20-bits internal format. In that case an additional
+	 *    (16) gain needs to be applied in HDR Decompression for the
+	 *    remaining 16-bit to 20-bit conversion.
+	 * 2) In HDR-merge mode there is the opposite issue where we want to
+	 *    keep the native sensor format up to the HDR-merge block. For that
+	 *    purpose LPALIGN0=0 is set to avoid PIPECONF rescaling. But it
+	 *    does not apply to that specific case so a (1/16) fractional gain
+	 *    needs to be set to revert the pixel format from 16-bit to 12-bit.
+	 */
 
-		hdrdec0Config->ctrl_enable = 1;
-
-		hdrdec0Config->knee_point1 = input0_.points[0];
-		hdrdec0Config->knee_point2 = input0_.points[1];
-		hdrdec0Config->knee_point3 = input0_.points[2];
-		hdrdec0Config->knee_point4 = input0_.points[3];
-
-		hdrdec0Config->knee_offset0 = input0_.offsets[0];
-		hdrdec0Config->knee_offset1 = input0_.offsets[1];
-		hdrdec0Config->knee_offset2 = input0_.offsets[2];
-		hdrdec0Config->knee_offset3 = input0_.offsets[3];
-		hdrdec0Config->knee_offset4 = input0_.offsets[4];
-
-		hdrdec0Config->knee_npoint0 = input0_.newpoints[0];
-		hdrdec0Config->knee_npoint1 = input0_.newpoints[1];
-		hdrdec0Config->knee_npoint2 = input0_.newpoints[2];
-		hdrdec0Config->knee_npoint3 = input0_.newpoints[3];
-		hdrdec0Config->knee_npoint4 = input0_.newpoints[4];
-
-		hdrdec0Config->knee_ratio0 = input0_.ratios[0];
-		hdrdec0Config->knee_ratio1 = input0_.ratios[1];
-		hdrdec0Config->knee_ratio2 = input0_.ratios[2];
-		hdrdec0Config->knee_ratio3 = input0_.ratios[3];
-		hdrdec0Config->knee_ratio4 = input0_.ratios[4];
+	IPAModeType &mode = context.configuration.pipelineMode;
+	unsigned int &hwRevision = context.hw.hwRevision;
+	if (!input0_.userConfig && bitsPerPixel_ == 12 && hwRevision == NEOISP_HW_V2) {
+		/* Update ratio[4] to amend the unitary gain (u7.5 format). */
+		if (mode != IPAModeTypeHdrMerge)
+			input0_.ratios[4] = (1 << 5) * 16;
+		else
+			input0_.ratios[4] = (1 << 5) / 16;
 	}
 
-	if (input1_.enabled) {
-		auto hdrdec1Config = params->block<BlockParamsType::HdrDec1>();
-		hdrdec1Config.setUpdate(true);
+	auto hdrdec0Config = params->block<BlockParamsType::HdrDec0>();
+	hdrdec0Config.setUpdate(true);
 
-		hdrdec1Config->ctrl_enable = 1;
+	hdrdec0Config->ctrl_enable = 1;
 
-		hdrdec1Config->knee_point1 = input1_.points[0];
-		hdrdec1Config->knee_point2 = input1_.points[1];
-		hdrdec1Config->knee_point3 = input1_.points[2];
-		hdrdec1Config->knee_point4 = input1_.points[3];
+	hdrdec0Config->knee_point1 = input0_.points[0];
+	hdrdec0Config->knee_point2 = input0_.points[1];
+	hdrdec0Config->knee_point3 = input0_.points[2];
+	hdrdec0Config->knee_point4 = input0_.points[3];
 
-		hdrdec1Config->knee_offset0 = input1_.offsets[0];
-		hdrdec1Config->knee_offset1 = input1_.offsets[1];
-		hdrdec1Config->knee_offset2 = input1_.offsets[2];
-		hdrdec1Config->knee_offset3 = input1_.offsets[3];
-		hdrdec1Config->knee_offset4 = input1_.offsets[4];
+	hdrdec0Config->knee_offset0 = input0_.offsets[0];
+	hdrdec0Config->knee_offset1 = input0_.offsets[1];
+	hdrdec0Config->knee_offset2 = input0_.offsets[2];
+	hdrdec0Config->knee_offset3 = input0_.offsets[3];
+	hdrdec0Config->knee_offset4 = input0_.offsets[4];
 
-		hdrdec1Config->knee_npoint0 = input1_.newpoints[0];
-		hdrdec1Config->knee_npoint1 = input1_.newpoints[1];
-		hdrdec1Config->knee_npoint2 = input1_.newpoints[2];
-		hdrdec1Config->knee_npoint3 = input1_.newpoints[3];
-		hdrdec1Config->knee_npoint4 = input1_.newpoints[4];
+	hdrdec0Config->knee_npoint0 = input0_.newpoints[0];
+	hdrdec0Config->knee_npoint1 = input0_.newpoints[1];
+	hdrdec0Config->knee_npoint2 = input0_.newpoints[2];
+	hdrdec0Config->knee_npoint3 = input0_.newpoints[3];
+	hdrdec0Config->knee_npoint4 = input0_.newpoints[4];
 
-		hdrdec1Config->knee_ratio0 = input1_.ratios[0];
-		hdrdec1Config->knee_ratio1 = input1_.ratios[1];
-		hdrdec1Config->knee_ratio2 = input1_.ratios[2];
-		hdrdec1Config->knee_ratio3 = input1_.ratios[3];
-		hdrdec1Config->knee_ratio4 = input1_.ratios[4];
-	}
+	hdrdec0Config->knee_ratio0 = input0_.ratios[0];
+	hdrdec0Config->knee_ratio1 = input0_.ratios[1];
+	hdrdec0Config->knee_ratio2 = input0_.ratios[2];
+	hdrdec0Config->knee_ratio3 = input0_.ratios[3];
+	hdrdec0Config->knee_ratio4 = input0_.ratios[4];
+
+	auto hdrdec1Config = params->block<BlockParamsType::HdrDec1>();
+	hdrdec1Config.setUpdate(true);
+
+	hdrdec1Config->ctrl_enable = 1;
+
+	hdrdec1Config->knee_point1 = input1_.points[0];
+	hdrdec1Config->knee_point2 = input1_.points[1];
+	hdrdec1Config->knee_point3 = input1_.points[2];
+	hdrdec1Config->knee_point4 = input1_.points[3];
+
+	hdrdec1Config->knee_offset0 = input1_.offsets[0];
+	hdrdec1Config->knee_offset1 = input1_.offsets[1];
+	hdrdec1Config->knee_offset2 = input1_.offsets[2];
+	hdrdec1Config->knee_offset3 = input1_.offsets[3];
+	hdrdec1Config->knee_offset4 = input1_.offsets[4];
+
+	hdrdec1Config->knee_npoint0 = input1_.newpoints[0];
+	hdrdec1Config->knee_npoint1 = input1_.newpoints[1];
+	hdrdec1Config->knee_npoint2 = input1_.newpoints[2];
+	hdrdec1Config->knee_npoint3 = input1_.newpoints[3];
+	hdrdec1Config->knee_npoint4 = input1_.newpoints[4];
+
+	hdrdec1Config->knee_ratio0 = input1_.ratios[0];
+	hdrdec1Config->knee_ratio1 = input1_.ratios[1];
+	hdrdec1Config->knee_ratio2 = input1_.ratios[2];
+	hdrdec1Config->knee_ratio3 = input1_.ratios[3];
+	hdrdec1Config->knee_ratio4 = input1_.ratios[4];
 }
 
 REGISTER_IPA_ALGORITHM(HdrDecomp, "HdrDecomp")

src/ipa/nxp/neo/algorithms/hdr_decomp.h
@@ -1,7 +1,7 @@
 /* SPDX-License-Identifier: LGPL-2.1-or-later */
 /*
  * hdr_decomp.h - NXP NEO HDR Decompression configuration
- * Copyright 2024 NXP
+ * Copyright 2024-2025 NXP
  */
 
 #pragma once
@@ -21,10 +21,11 @@ namespace ipa::nxpneo::algorithms {
 class HdrDecomp : public Algorithm
 {
 public:
-	HdrDecomp();
+	HdrDecomp(){};
 	~HdrDecomp() = default;
 
 	int init(IPAContext &context, const YamlObject &tuningData) override;
+	int configure(IPAContext &context, const IPACameraSensorInfo &configInfo) override;
 	void prepare(IPAContext &context, const uint32_t frame,
 		     IPAFrameContext &frameContext,
 		     NxpNeoParams *params) override;
@@ -36,20 +37,22 @@ private:
 	static constexpr size_t kNumRatios = 5;
 
 	struct {
-		bool enabled = false;
 		std::vector<uint16_t> points;
 		std::vector<uint16_t> offsets;
 		std::vector<uint32_t> newpoints;
 		std::vector<uint16_t> ratios;
+		bool userConfig = false;
 	} input0_;
 
 	struct {
-		bool enabled = false;
 		std::vector<uint16_t> points;
 		std::vector<uint16_t> offsets;
 		std::vector<uint16_t> newpoints;
 		std::vector<uint16_t> ratios;
+		bool userConfig = false;
 	} input1_;
+
+	unsigned int bitsPerPixel_ = 0;
 };
 
 } /* namespace ipa::nxpneo::algorithms */

commit 9a19508329f6d5603abe9ff6c8562893f133c3c8

ipa: neo: Fix typos in comments
This is a non-functional change that fixes typos in code comments.

Fixes: f79d40624527 ("ipa: neo: hdr_decomp: Add mode-dependent default configuration")
Fixes: 36f66d5f425b ("ipa: neo: pipe_conf: Set a LPALIGN default value dependency on mode")

Signed-off-by: Julien Vuillaumier <julien.vuillaumier@nxp.com>

src/ipa/nxp/neo/algorithms/hdr_decomp.cpp
@@ -263,7 +263,7 @@ void HdrDecomp::prepare(IPAContext &context, const uint32_t frame,
 	 * related to PIPECONF.LPALIGN0 setting with 12-bit sensor pixel format:
 	 * - Rescaling is done to 16-bit instead of 20-bit for other sensor
 	 *   formats
-	 * - Rescaling is applied event though LPALIGN=0
+	 * - Rescaling is applied even though LPALIGN=0
 	 * This leads to 2 exceptions on this ISP revision V2 with input0 12-bit
 	 * pixel format:
 	 * 1) In non HDR-merge mode, we rely on PIPECONF.LPALIGN0/1 to rescale

src/ipa/nxp/neo/algorithms/pipe_conf.cpp
@@ -105,7 +105,7 @@ namespace ipa::nxpneo::algorithms {
  * |  16  |  16   |  16   |  20   |  20   |
  * +------+-------+-------+-------+-------+
  *
- * input1 (INALIGN1)
+ * input1 (LPALIGN1)
  * +------+---------------+---------------+
  * |      | LPALIGN1 = 0  | LPALIGN1 = 1  |
  * | ibpp +-------+-------+-------+-------+
@@ -125,7 +125,7 @@ namespace ipa::nxpneo::algorithms {
  * entries, those are applied with priority. If not configured, the algorithm
  * falls back into automatic configuration mode using the following logic:
  * - For non HDR-merge mode of operation, configure LPALIGN0/1=1
- * - For HDR merge of operation, configure LPALIGN0/1=0 to keep the native
+ * - For HDR-merge mode of operation, configure LPALIGN0/1=0 to keep the native
  *   camera pixel format, as required for the HDR merge block.
  *
  * Note: for non-linear pixel format decompression using HDR Decompression unit,